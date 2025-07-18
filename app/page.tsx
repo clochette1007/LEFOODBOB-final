114aec2927d19b7af127376b1a7d38c63be99722
MacBook <macbook@MacBook-Pro-de-MacBook.local>
2025-07-18 11:23:33 +0200

    ajout barre navigation mobile responsive accueil

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import RestaurantMapWithAdmin from "../components/restaurant-map-with-admin"
+import MobileNav from "../components/mobile-nav"
 
 export default function Page() {
-  return <RestaurantMapWithAdmin />
+  return (
+    <>
+      <RestaurantMapWithAdmin />
+      <MobileNav />
+    </>
+  )
 }
